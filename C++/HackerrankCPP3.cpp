f38111926f1c9c2f43ce34e04ac036e50a68de4b
Ajayjam97 <jamwal_ajay@hotmail.com>
2020-12-27 22:14:01 +0530

    Hackerrank Q3

---
+++ C++/HackerrankCPP3.cpp
@@ -0,0 +1,22 @@
+//https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long long int l;
+    char c;
+    float f;
+    double d;
+    cin>>i;
+    cin>>l;
+    cin>>c;
+    cin>>f;
+    cin>>d;
+    cout<<i<<endl<<l<<endl<<c<<endl;
+    cout<<fixed<<setprecision(3)<<f<<endl;
+    cout<<fixed<<setprecision(9)<<d<<endl;
+    return 0;
+}
